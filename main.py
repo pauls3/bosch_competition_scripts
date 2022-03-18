bd24c08d15f9661478b64730ba86d68bc2d9bf63
Paul Stanik <paulstanik3@gmail.com>
2022-03-17 19:00:31 -0700

    created script to edit image filepaths in xml files

---
+++ main.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+import os
+
+
+directory = 'home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        tree = ET.parse(f)
+        root = tree.getroot()
+
+        img_filename = ""
+        #ET.SubElement(root[0], 'filename')
+        for element in root.findall('filename'):
+            img_filename = element.attrib
+        
+        if img_filename != "":
+            for element in root.findall('path'):
+                element.text = 'home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations' + img_filename
+
+            tree.write(filename)
+        else:
+            continue

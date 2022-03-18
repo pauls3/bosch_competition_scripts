1b3d7bc28b4e700d0186bd2743682debbbc3a0bc
Paul Stanik <paulstanik3@gmail.com>
2022-03-17 19:02:12 -0700

    fixed file paths

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 
 
-directory = 'home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations'
+directory = '/home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations'
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
@@ -16,7 +16,7 @@ for filename in os.listdir(directory):
         
         if img_filename != "":
             for element in root.findall('path'):
-                element.text = 'home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations' + img_filename
+                element.text = '/home/stanik/rtis_lab/data/bosch_object_detection_midterm/annotations' + img_filename
 
             tree.write(filename)
         else:

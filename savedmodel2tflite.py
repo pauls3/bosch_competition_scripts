36237b0fb50581869973a40301b5e87cfddd9514
Paul Stanik <paulstanik3@gmail.com>
2022-03-18 17:41:54 -0700

    trying to make tflite model file

--- savedmodel2tflite.py
+++ savedmodel2tflite.py
@@ -1,14 +1,14 @@
-# import tensorflow as tf
-
-# # Convert the model.
-# converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_0/saved_model')
-# tflite_model = converter.convert()
-
-# # Save the model.
-# with open('model.tflite', 'wb') as f:
-#   f.write(tflite_model)
-
 import tensorflow as tf
-converter = tf.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_0/saved_model')
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-tflite_quant_model = converter.convert()
+
+# Convert the model.
+converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_0/saved_model')
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
+# import tensorflow as tf
+# converter = tf.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_0/saved_model')
+# converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# tflite_quant_model = converter.convert()

5bdfa16d89d4c7f2de9354dab095f2195bc4aaec
Paul Stanik <paulstanik3@gmail.com>
2022-03-18 17:43:40 -0700

    trying tflite conversion again with different model

--- savedmodel2tflite.py
+++ savedmodel2tflite.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Convert the model.
-converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_0/saved_model')
+converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model('/home/stanik/repos/TensorFlow_1.13/workspace/training_demo/training/tf_1')
 tflite_model = converter.convert()
 
 # Save the model.
